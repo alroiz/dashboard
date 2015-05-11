1c05fed9faeb98dfaa2e55b63b16f3b30b4907aa
alroiz <alroiz@gmail.com>
2015-05-11 14:44:03 +0200

    secondCommit

--- config/env/development.js
+++ config/env/development.js
@@ -2,5 +2,6 @@ var port = 3002;
 
 module.exports = {
 	port: port,
-	db: 'mongodb://localhost/statsPortalCVTE'
+	//db: 'mongodb://localhost/statsPortalCVTE'
+	db: 'mongodb://magicbox:magicbox@proximus.modulusmongo.net:27017/one5Tevu'
 };
